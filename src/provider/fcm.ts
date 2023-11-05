8dced5ef3be58ff725ed2724ebf10dbe3b9457ec
Bastian Dietrich <bastian@dietr.de>
2023-11-05 20:26:00 +0100

    fcm

--- src/provider/fcm.ts
+++ src/provider/fcm.ts
@@ -1,36 +1,41 @@
-import { ProviderResult, Notification, Provider, ProviderConfiguration } from '../interfaces';
-
-const sendNotification = (notification: object, deviceToken: string, apiToken: string, projectId: string) => 
-  fetch('https://fcm.googleapis.com/v1/projects/' + projectId + '/messages:send', {
-    method: 'POST',
-    body: JSON.stringify({ message: {notification: notification}, token: deviceToken }),
-    headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + apiToken },
-  }); 
+import { Messaging, NotificationMessagePayload, getMessaging } from 'firebase-admin/messaging';
+import { Notification, Provider, ProviderConfiguration, Device } from '../interfaces';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
 
 // structure of the configuration for this provider
-export interface FirebaseConfiguration extends ProviderConfiguration {
-  apiKey?: string;
-  projectId?: string;
-}
+export interface FirebaseConfiguration extends ProviderConfiguration {}
 
 export class FirebaseProvider extends Provider {
-  private client: (notification: {body: string; data: string;}, deviceToken: string) => Promise<Response>;
+  private client: Messaging;
 
   constructor(config: FirebaseConfiguration) {
     super(config);
 
     // check the configuration
-    if (!(config.apiKey && config.projectId)) {
+    if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
       console.error('Misconfigured firebase provider.');
       process.exit(1);
     }
 
     // create fcm client
-    this.client = (notification: object, deviceToken: string) => 
-      sendNotification(notification, deviceToken, config.apiKey!, config.projectId!);
+    const app = initializeApp({
+      credential: applicationDefault()
+    });
+
+    this.client = getMessaging(app);
   }
 
-  async send(request: Notification): Promise<ProviderResult> {
-    return Promise.reject('Not implemented!');
+  async send(request: Notification, device: Device): Promise<boolean> {
+    const newNotification: NotificationMessagePayload = {};
+
+    newNotification.title = request.sender_display_name;
+    newNotification.body = (request.content?.body as string) ?? 'New Mesage';
+    
+    const result = await this.client.send({
+      token: device.pushkey,
+      notification: newNotification,
+    });
+
+    return result.length !== 0;
   }
 }
